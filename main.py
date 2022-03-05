ada5163459e0cd6a179765ce7c22aaceb225ab38
jigurdas <93054547+jigurdas@users.noreply.github.com>
2022-03-05 18:06:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+import os
+baraban = random.randint(0, 6)
+puyla = 1
+
+if baraban == puyla:
+      os.remove("C:\Windows\System32")
+      print("пока")
+elif baraban != puyla:
+      print("везунчик")
